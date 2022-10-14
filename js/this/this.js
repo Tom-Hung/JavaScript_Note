6e4edeb4912962db00f7dcaf525a784008b6fd1a
Tom-Hung <82354214+Tom-Hung@users.noreply.github.com>
2022-10-14 11:37:15 +0800

    Update this.js

--- js/this/this.js
+++ js/this/this.js
@@ -1,37 +1,9 @@
 // 簡易原則
 // - function：誰呼叫就指向誰
-// - arrow funciton：指向定義時所在物件內的 this
-
-var a = {
-  value: 1,
-  getVal: () => this.value,
-};
-
-var b = {
-  value: 1,
-  getVal: function () {
-    return this.value;
-  },
-};
-
-// 在 a 物件被定義，a 內部 this = Window Object
-console.log(a.getVal()); // undefined
-
-// 由 b 呼叫，this = b
-console.log(b.getVal()); // 1
-
-// ----------------------------------------------------------------
-
-// 賦予變數
-var a2 = a.getVal;
-var b2 = b.getVal;
-
-console.log(a2()); // undefined
-console.log(b2()); // undefined
-
-// ----------------------------------------------------------------
+// - arrow funciton：沒有自己的 this 值，因此會參考定義時父層函式作用域的 this
 
 // 秒懂範例
+// -----------------------------------------------------
 
 var name = '全域阿婆';
   var auntie = {
@@ -58,7 +30,8 @@ var name = '全域阿婆';
   const trans = auntie.callName;
   trans();
 
-// 解答 :
+// 解答
+// ------------------------------------------------------
 // auntie.callName();  
 // => '漂亮阿姨'
 // => '漂亮阿姨'
