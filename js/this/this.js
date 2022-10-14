f3366e5c9d93952bcbcc8b3492f456858da5c32d
Tom-Hung <82354214+Tom-Hung@users.noreply.github.com>
2022-10-14 11:35:56 +0800

    Update this.js

--- js/this/this.js
+++ js/this/this.js
@@ -28,3 +28,48 @@ var b2 = b.getVal;
 
 console.log(a2()); // undefined
 console.log(b2()); // undefined
+
+// ----------------------------------------------------------------
+
+// 秒懂範例
+
+var name = '全域阿婆';
+  var auntie = {
+    name: '漂亮阿姨',
+    callName: function () {
+      console.log('1', this.name);
+      setTimeout(() => {
+        console.log('2', this.name); 
+        console.log('3', this); 
+      }, 10);
+    },
+    callName2: () => {
+      console.log('4', this.name);
+      setTimeout(() => {
+        console.log('5', this.name); 
+        console.log('6', this); 
+      }, 10);
+    },
+  };
+
+  auntie.callName(); 
+  auntie.callName2();
+
+  const trans = auntie.callName;
+  trans();
+
+// 解答 :
+// auntie.callName();  
+// => '漂亮阿姨'
+// => '漂亮阿姨'
+// => auntie 物件
+
+// auntie.callName2();
+// => '全域阿婆'
+// => '全域阿婆'
+// => window or global 物件
+
+// trans();
+// => '全域阿婆'
+// => '全域阿婆'
+// => window or global 物件
